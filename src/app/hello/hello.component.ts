ff22f8035cfc79a5d7afd5202e34592f1ca283ae
trieu.le <vinhtrieulnvt@gmail.com>
2023-07-15 10:16:48 +0700

    day 04

---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+import { E } from "@angular/core/src/render3";
+
+@Component({
+    selector: 'Hello',
+    templateUrl: 'hello.Component.html',
+})
+export class HelloComponent{
+    Num1 = 199;
+    Num2 = 20;
+    Num3 = 0;
+
+    arr = [2,55,3,67,123];
+}
